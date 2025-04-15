aec4d26d9fc5147c6b884c379e94f9f0da578384
WilDev-fp <wiliscuola@gmail.com>
2025-04-15 18:11:34 +0200

    Test in RepositoryBase

--- RentalCarsWithRepositoryDesignPattern/Infrastructure/Repositories/RepositoryBase.cs
+++ RentalCarsWithRepositoryDesignPattern/Infrastructure/Repositories/RepositoryBase.cs
@@ -1,5 +1,17 @@
-﻿namespace RentalCarsWithRepositoryDesignPattern.Infrastructure.Repositories;
+﻿using RentalCarsWithRepositoryDesignPattern.Application.IRepositories;
+using RentalCarsWithRepositoryDesignPattern.Infrastructure.Context;
 
-public abstract class RepositoryBase
+namespace RentalCarsWithRepositoryDesignPattern.Infrastructure.Repositories;
+
+public abstract class RepositoryBase<T> : IRepositoryBase<T>
 {
+    protected RepositoryBase(RentalCarContext rentalCarContext)
+    {
+    }
+
+    public abstract bool Delete(long id);
+    public abstract IEnumerable<T> GetAll();
+    public abstract T GetById(long id);
+    public abstract bool Insert(T item);
+    public abstract bool Update(T item);
 }
