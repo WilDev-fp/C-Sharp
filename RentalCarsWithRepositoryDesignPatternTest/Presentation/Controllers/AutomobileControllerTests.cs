28acac7418efadd7c6b280c2ae81b4d4e83f1a94
WilDev-fp <wiliscuola@gmail.com>
2025-05-02 18:01:33 +0200

    Add class for first Test.

---
+++ RentalCarsWithRepositoryDesignPatternTest/Presentation/Controllers/AutomobileControllerTests.cs
@@ -0,0 +1,10 @@
+﻿namespace RentalCarsWithRepositoryDesignPattern.Presentation.Controllers;
+
+public class AutomobileControllerTests
+{
+    [Fact]
+    public void addTest()
+    {
+        Assert.True(true);
+    }
+}
